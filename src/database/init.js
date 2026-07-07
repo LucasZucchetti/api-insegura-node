4e681bfd427df76253a4e8815d5ec89ea78552f6
Lucas Zucchetti <dev.lucaszucchetti@gmail.com>
2026-07-07 17:27:24 -0300

    feat(database): Add unique constraint to user email

--- src/database/init.js
+++ src/database/init.js
@@ -1,18 +1,16 @@
 import conectar from "./connection.js";
 
 (async () => {
+  const db = await conectar();
 
-    const db = await conectar();
-
-    await db.exec(`
-        CREATE TABLE IF NOT EXISTS usuarios(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT NOT NULL,
-            email TEXT NOT NULL,
-            senha TEXT NOT NULL
-        );
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    senha TEXT NOT NULL
+    );
     `);
 
-    console.log("Banco inicializado.");
-
+  console.log("Banco inicializado.");
 })();
